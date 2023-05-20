81eceb623b63aa3fb02bdbe96e3d8bc50c5521cf
Vidroazra <105456494+Vidroazra@users.noreply.github.com>
2023-05-20 18:02:54 +0200

    Add files via upload

--- TEC/src/Tarea1.py
+++ TEC/src/Tarea1.py
@@ -16,6 +16,9 @@ led = PWM(Pin(14))
 signal_in = ADC(26)
 signal_out = PWM(Pin(14))
 signal_out.freq(10000)
+#buffer = [0,0,0,0,0,0,0,0,0,0] #cambio y creación de buffer
+N = 5
+buffer = []
 
 # ------------------------------------------------------------------------------
 # ADC
@@ -45,6 +48,51 @@ def parse_command(cmd):
 #   2. Genera la siguiente muestra de la señal y la envía a la salida (PWM).
 #   3. Lee el valor de la entrada analógica (ADC).
 # ------------------------------------------------------------------------------
+def FFT(buffer):
+    #N = 5
+    #buffer = [0 for n in range(N)] => esto debería crear el buffer = [0,0,0,0,0,0...hasta N]
+    for n in range (N):
+        for k in range (N):
+            Wkn[n][k] = cmath.exp(-1j*2*cmath.pi*k*n/N)
+            
+            
+            
+    #Genero las funciones par e impar x(2n) y x(2n+1), respectivamente:
+    fn = [0 for n in range(N)] #create a list of zeros 
+    gn = [0 for n in range(N)] #no necesario 
+    for i in range(2*N):
+        if i % 2 == 0: #par
+            n = int(i/2)
+            fn[n] = buffer[i]
+        else: #impar
+            n = int((i-1)/2)
+            gn[n] = buffer[i]
+            
+    #Se calcula la DFT de las funciones par e impar: f(n) y g(n) F(k) y G(k).
+    #El cálculo lo hacemos a través de la matriz de coeficientes Wkn
+    #fk = [0 for n in range(N)]
+    fk[k] = 0
+    gk[k] = 0
+    for k in range(N):
+        for n in range(N):
+        fk[k] = fn[k]*Wkn[n][k]+fk[k]
+        gk[k] = gn[k]*Wkn[n][k] + gk[k]
+
+    #Finalmente, calculamos la trans. Fourier de la señal completa X[k] al sumar F[k]+G[k]
+    #Hay que tener en cuenta que la fórmula cambia, dependiendo en que rango nos encotramos
+    #Para 0<=k<=N/2, la fórmula es: X[k] = F[k] + W*G[k]
+    #Para N/2<=k<=N, la fórmula es: X[k] = F[k] - W*G[k]
+    #Primero calculamos la matriz W:
+    
+              
+
+
+
+
+
+
+
+
 def waitNextPeriod(previous):
     lapsed = time.ticks_us() - previous
     offset = -60
@@ -66,6 +114,16 @@ def loop():
               t = waitNextPeriod(tLast)
               u = signal((t-t0)*1e-6)
               y = readInput()
+              
+              
+              buffer[i] = y
+              yjw = FFT(buffer)
+              for j in range(2*N):
+                  ywn = abs(yjw[j])
+                  print(f'{u} {y}')
+                  
+                  
+                  
               writeOutput(u)
             except ValueError:
               pass
@@ -74,7 +132,7 @@ def loop():
         if spoll.poll(0):
             cmd = str(sys.stdin.readline())
             parse_command(cmd)
-        print(f'{u} {y}')
+        #print(f'{u} {y}')
 # ------------------------------------------------------------------------------
 # INSTRUCCIONES
 # ------------------------------------------------------------------------------
@@ -83,17 +141,13 @@ BUFFER_SIZE = 10 # Muestras en el buffer
 
 def signal(t):
   # Pon aquí el código necesario para generar tu señal.
-  return math.sin(2*math.pi*1000*t)
-  return 1
+  
+  signal_o = math.sin(2*math.pi*1000*t)
+  return int(math.fabs(signal_o))
 
 # ------------------------------------------------------------------------------
 # Comienza la ejecución
 # ------------------------------------------------------------------------------
 loop()
-while True:
-    try:
-        y = readInput()
-        except
-            ValueError
-        pass
+
     
